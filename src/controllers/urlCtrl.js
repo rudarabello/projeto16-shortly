65b705c35346950c2264121e4a3b84f6d53ebfc4
Ruda <ruda.rabello.silva@gmail.com>
2022-08-05 17:44:27 -0300

    feat getUrlById

--- src/controllers/urlCtrl.js
+++ src/controllers/urlCtrl.js
@@ -16,3 +16,23 @@ export async function urlShortenControll(req, res) {
         res.sendStatus(500);
     }
 }
+
+export async function getUrlById(req, res) {
+    const id = parseInt(req.params.id);
+    if (isNaN(id)) {
+        return res.sendStatus(422);
+    }
+    try {
+        const { rows: response } = await clientPg.query(`
+        SELECT id, url, "shortUrl" FROM "shortenUrls"
+        WHERE id = $1
+        `, [id]);
+        if (response.length < 1) {
+            res.sendStatus(404);
+        }
+        res.status(200).send(response[0]);
+    } catch (error) {
+        console.log(error);
+        res.sendStatus(500);
+    }
+}
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,9 +1,10 @@
 import { Router } from 'express';
-import { signUpController, signInController } from '../controllers/loginAndRegisterCTRL.js';
+import { signUpController, signInController } from '../controllers/loginSignCTRL.js';
 import { signUpMiddle, signInMiddle } from '../middlewares/loginAndRegisterMW.js';
 import { verifyTokenMW } from '../middlewares/verifyTokenMW.js';
 import { urlShortenMW } from '../middlewares/urlShortenMW.js';
-import { urlShortenControll } from '../controllers/urlShortenCTRL.js';
+import { urlShortenControll, getUrlById } from '../controllers/urlCTRL.js';
+
 
 const route = Router();
 
@@ -11,6 +12,8 @@ route.post('/signup', signUpMiddle, signUpController);
 route.post('/signin', signInMiddle, signInController);
 
 route.post('/urls/shorten', verifyTokenMW, urlShortenMW, urlShortenControll);
+
+route.get('/urls/:id', getUrlById);
 
 
 export default route;
